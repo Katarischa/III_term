ee111429d22507568bdaf4fe39b4d564c6ef42f0
Katarischa <bichina.ma@phystech.edu>
2021-09-08 22:12:55 +0300

    third_task

--- task_3.cpp
+++ task_3.cpp
@@ -10,18 +10,18 @@ int main()
         {D = b*b - 4*a*c;
         if (D == 0)
             {x_1 = (-b + sqrt(D))/(2*a);
-            std:: cout << x_1;}
+            std:: cout <<"Solve is " << x_1;}
         if (D > 0)
             {x_1 = (-b + sqrt(D))/(2*a);
              x_2 = (-b - sqrt(D))/(2*a);
-            std:: cout << x_1 <<"  "<< x_2;}
+            std:: cout <<"Solve is " << x_1 <<" and "<< x_2;}
         if (D < 0)
             {std::cout << "We haven't some solve(";}
         }
     if (a == 0) {
             if (b != 0)
             {x_1 = c / b;
-            std::cout << x_1;}
+            std::cout <<"Solve is " << x_1;}
         if (b == 0 && c == 0) { std::cout << "All x is solve"; }
         if (b == 0 && c != 0) { std::cout << "We haven't some solve("; }
     }
