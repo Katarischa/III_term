798ef7822ed4df393539ce73044ea1911172c708
Katarischa <bichina.ma@phystech.edu>
2021-09-09 14:03:45 +0300

    HW_1_done

---
+++ cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/main.cpp.obj"
+  "CMakeFiles/main.cpp.dir/task_2.cpp.obj"
+  "CMakeFiles/main.cpp.dir/task_3.cpp.obj"
+  "CMakeFiles/main.cpp.dir/task_4.cpp.obj"
+  "libmain.cpp.dll.a"
+  "main.cpp.exe"
+  "main.cpp.exe.manifest"
+  "main.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/task_3.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_3.cpp.dir/task_3.cpp.obj"
+  "libtask_3.cpp.dll.a"
+  "task_3.cpp.exe"
+  "task_3.cpp.exe.manifest"
+  "task_3.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_3.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
