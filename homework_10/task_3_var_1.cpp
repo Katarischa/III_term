8ae0926bc7cb295db82289ab63f232d772d31f15
Katarischa <bichina.ma@phystech.edu>
2021-11-29 10:14:42 +0300

    3_task_10_and_answer_11

--- homework_10/task_3_var_1.cpp
+++ homework_10/task_3_var_1.cpp
@@ -4,42 +4,22 @@
 
 #include <iostream>
 
-//template<typename T, typename... Types>
-//unsigned size(unsigned sum, T first_argument, Types... arguments) {
-//    sum += sizeof(first_argument);
-//    if constexpr(sizeof...(arguments) > 0) {
-//        size(sum, arguments...);
-//    }
-//        return sum;
-//    }
-//
-
-//template<typename T, typename... Types>
-//unsigned Size1(T first_argument, Types... arguments) {
-//    unsigned sum = 0;
-//    sum += sizeof(first_argument);
-//    if constexpr(sizeof...(arguments) > 0) {
-//        Size1(first_argument, arguments...);
-//    }
-//    return sum;
-//}
-
 template<typename T, typename... Types>
-unsigned Size1(unsigned sum, T first_argument, Types... arguments) {
+unsigned size(unsigned sum, T first_argument, Types... arguments) {
     sum += sizeof(first_argument);
     if constexpr(sizeof...(arguments) > 0) {
-        Size1(sum, arguments...);
-    }
-    else{
+        size(sum, arguments...);
+    } else {
         return sum;
     }
+
 }
 
 
 int main() {
-    unsigned int a;
-    a = Size1(45, 1, 1, 2, "sd", 5);
-    std::cout << a << std::endl;
+    unsigned int array_of_data;
+    array_of_data = size(45, 2, 1, 2, "Hello,my dear friend)", '3', 2, 5);
+    std::cout << array_of_data << std::endl;
 
     return 0;
 }
